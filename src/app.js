313df90e3c5b3704e6299ddff98d1935771e7aa1
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-04-23 10:46:08 +0100

    Created home page

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bulma'
 import './style.scss'
+import { HashRouter, Route, Link } from 'react-router-dom'
+
+import Home from './components/Home'
 
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <HashRouter>
+        <main>
+          <h1>Hello World!</h1>
+          <Route path="/" component={Home} />
+        </main>
+      </HashRouter>
     )
   }
 }
