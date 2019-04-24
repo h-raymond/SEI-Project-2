8ddfb799a58086fb58d8500b52b14407e927d9c3
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-04-24 15:04:49 +0100

    Styling updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <section className="section">
       <div className="container">
-        <img className="homeLogo" src="../images/artboard1.png" />
-        <h2 className="subtitle is-2">A football club comparison site</h2>
+        <img className="homeLogo" src="../images/Versus.png" />
+        <h2 className="subtitle is-2">A Premier League comparison site</h2>
       </div>
     </section>
   )
